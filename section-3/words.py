bb3311e3b5b4b78c4161f3456129a18d5b106641
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-04-15 14:58:52 +0100

    A plain def export cannot be used to execute from OS shell

--- section-3/words.py
+++ section-3/words.py
@@ -1,14 +1,22 @@
-# run from REPL using 
-# python words.py
-# 'import words' in REPL would also execute the code, printing the results which might not be 
-#       what we want
-from urllib.request import urlopen
-with urlopen('http://sixty-north.com/c/t.txt') as story:
-    story_words = []
-    for line in story:
-        line_words = line.decode('utf-8').split()
-        for word in line_words:
-            story_words.append(word)
+# 'import words' in REPL would now only import the fetch_words definition
+#       giving us more control to run as a program rather than a script
+# like so - 
+# import words
+# words.fetch_words()
+# OR
+# from words import fetch_words
+# fetch_words()
+# NOW this can now ONLY be used as import and run as part of another program
+# CANNOT be run directly in OS shell using 'python words.py' - this would simply
+# define the fetch_words() function and exit immediately
+def fetch_words():
+    from urllib.request import urlopen
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
 
-for word in story_words:
-    print(word)
+    for word in story_words:
+        print(word)

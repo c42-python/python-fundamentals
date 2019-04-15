7a89db40f2d0e4a55819da485748409d0c02c3b9
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-04-15 15:34:12 +0100

    Documenting code - so can play nicely with help()

--- section-3/words.py
+++ section-3/words.py
@@ -1,8 +1,22 @@
+""" Retrive and print words from an URL.
+
+Usage:
+    python words.py <URL>
+"""
 import sys
 
 # using an entry function to easily use the code as script (called main here)
 # otherwise can be imported to use function defs individually in other programs
 def fetch_words(url):
+    """ Fetch a list of words from an URL.
+
+    Args:
+        url: The URL of an UTF-8 text document.
+
+    Returns:
+        A list of strings containing the words from the 
+        document
+    """
     from urllib.request import urlopen
     with urlopen(url) as story:
         story_words = []
@@ -14,15 +28,26 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """ Print items one per line
+
+    Args:
+        An iterable series of printable items 
+    """
     for item in items:
         print(item)
 
 
 # Easily pass function args via REPL
 def main(url):
+    """ Print each word from a text document from an URL
+    
+    Args:
+        url: The url of a UTF-8 text document.
+
+    """
     words = fetch_words(url)
     print(words)
-    
+
 
 # Pass params in script execution as command line positinal args
 if __name__ == '__main__':

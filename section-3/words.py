e31ac5f79a070a6595b30fb88096c6a5f3d742dc
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-04-15 15:16:20 +0100

    Using a 'main' wrapper

--- section-3/words.py
+++ section-3/words.py
@@ -1,14 +1,5 @@
-# 'import words' in REPL would now only import the fetch_words definition
-#       giving us more control to run as a program rather than a script
-# like so - 
-# import words
-# words.fetch_words()
-# OR
-# from words import fetch_words
-# fetch_words()
-# NOW this can now ONLY be used as import and run as part of another program
-# CANNOT be run directly in OS shell using 'python words.py' - this would simply
-# define the fetch_words() function and exit immediately
+# using an entry function to easily use the code as script (called main here)
+# otherwise can be imported to use function defs individually in other programs
 def fetch_words():
     from urllib.request import urlopen
     with urlopen('http://sixty-north.com/c/t.txt') as story:
@@ -17,11 +8,17 @@ def fetch_words():
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
+    return story_words
 
-    for word in story_words:
-        print(word)
+def print_items(items):
+    for item in items:
+        print(item)
+
+def main():
+    words = fetch_words()
+    print(words)
 
 # if executed as a script then call the function and execute it
 # which means when run as an import only the function would be defined - not executed immediately
 if __name__ == '__main__':
-    fetch_words()
+    main()

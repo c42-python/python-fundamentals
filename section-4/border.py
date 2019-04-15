89687ffa1902436700f79ce650a2386a0c939457
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2019-04-15 17:00:47 +0100

    Interesting code

---
+++ section-4/border.py
@@ -0,0 +1,5 @@
+def banner(message, border='-'):
+    line = border * len(message) # Repetition operation
+    print(line)
+    print(message)
+    print(line)
---
+++ section-4/param_defaults.py
@@ -0,0 +1,8 @@
+import sys
+
+def add_spam(menu=[]):
+    menu.append('spam')
+    print(menu)
+
+# if __name__ == '__main__':
+#     add_spam(sys.argv[1])
